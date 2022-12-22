ab79eec9da032fbe8bc9e551a3dc1fdd42aea499
ody0401 <ody0401@naver.com>
2022-12-22 20:17:17 +0900

    second

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import postReducer from '../slice/post';
 import userReducer from '../slice/user';
 
+const isDev = process.env.NODE_ENV === 'development';
 export const store = configureStore({
   reducer: {
     post: postReducer,
     user: userReducer,
   },
+  devTools: isDev,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
